3a6b9b97443938bf0a3c0ed113c064f98f341c42
Nattawut <nattawut@thinknet.co.th>
2017-03-20 14:50:55 +0700

    add test NewTodo

---
+++ src/components/__testes__/NewTodo.test.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import NewTodo from '../NewTodo'
+
+describe('NewTodo', () => {
+  let component
+  let props
+
+  beforeEach(() => {
+    component = shallow(<NewTodo />)
+  })
+
+  it('renders correct structure', () => {
+    expect(component.is('div')).toBe(true)
+    expect(component.hasClass('new-todo')).toBe(true)
+    expect(component.find('input').length).toBe(1)
+    expect(component.find('button').length).toBe(1)
+    expect(component.find('button').first().hasClass('btn'))
+    expect(component.find('button').first().hasClass('btn-primary'))
+  })
+
+  it('matches its snapshot', () => {
+    expect(component).toMatchSnapshot()
+  })
+
+  it('changes state when typing', () => {
+    component.find('input').simulate('change', {
+      target: {
+        value: 'Hello',
+      },
+    })
+    expect(component.state('text')).toBe('Hello')
+  })
+})

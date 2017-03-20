8e841257ae66c35103b5eef7b0c726a4900668ed
Nattawut <nattawut@thinknet.co.th>
2017-03-20 13:18:20 +0700

    add TodoList (TDD)

---
+++ src/components/TodoList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TodoList = props => (
+  <ul className="todo-list" />
+)
+
+export default TodoList
---
+++ src/components/__tests__/TodoList.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import TodoList from '../TodoList'
+
+describe('TodoList', () => {
+  let component
+  let props
+
+  beforeEach(() => {
+    props = {
+      todos: [
+        { id: 1, text: 'Feed cat', complete: false },
+        { id: 2, text: 'Drink coffee', complete: true },
+      ],
+    }
+    component = shallow(<TodoList {...props} />)
+  })
+
+  it('renders correct structure', () => {
+    expect(component.is('ul')).toBe(true)
+    expect(component.hasClass('todo-list')).toBe(true)
+  })
+})

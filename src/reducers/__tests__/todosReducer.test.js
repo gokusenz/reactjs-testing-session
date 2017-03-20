f6c455f9f655152ef23da05d899a1704629dbc43
Nattawut <nattawut@thinknet.co.th>
2017-03-20 13:30:22 +0700

    add todosReduer test

--- src/reducers/__tests__/todosReducer.test.js
+++ src/reducers/__tests__/todosReducer.test.js
@@ -4,4 +4,13 @@ describe('todosReducer', () => {
   it('return correct initial state', () => {
     expect(todosReducer(undefined, {})).toEqual([])
   })
+
+  it('returns current state if no action matched', () => {
+    const curState = [
+      { id: 1, text: 'Feed cat', complete: false },
+    ]
+    const nextState = todosReducer(curState, {})
+    expect(nextState).toBe(curState) // compare pointers
+    expect(nextState).toEqual(curState) // compare values
+  })
 })

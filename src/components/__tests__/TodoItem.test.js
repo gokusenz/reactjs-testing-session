0ebaae963e0ed4ce56c0fe4d9b4b6f0ffb2cde92
Nattawut <nattawut@thinknet.co.th>
2017-03-20 11:20:29 +0700

    add TodoItem Test

--- src/components/__tests__/TodoItem.test.js
+++ src/components/__tests__/TodoItem.test.js
@@ -1,11 +1,11 @@
-// Describe what to test
+import React from 'react'
+import { shallow } from 'enzyme'
+import TodoItem from '../TodoItem'
+
 describe('TodoItem', () => {
-  // Test scenario
-  it('runs jest', () => {
-    expect(5).toBe(5)
-  })
-  // Test scenario
-  it('checks array', () => {
-    expect([1, 2, 3]).toEqual([1, 2, 3])
+  it('renders correct structure', () => {
+    const component = shallow(<TodoItem text="Hello" />)
+
+    expect(component.is('li')).toBe(true)
   })
 })

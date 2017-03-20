b25ce96bd324bdb9fd03b0ad56124c2819517c3b
Nattawut <nattawut@thinknet.co.th>
2017-03-20 11:40:58 +0700

    comment sample test

--- src/components/__tests__/Sample.test.js
+++ src/components/__tests__/Sample.test.js
@@ -1,11 +1,11 @@
-// Describe what to test
-describe('Sample Test', () => {
-  // Test scenario
-  it('runs jest', () => {
-    expect(5).toBe(5)
-  })
-  // Test scenario
-  it('checks array', () => {
-    expect([1, 2, 3]).toEqual([1, 2, 3])
-  })
-})
+// // Describe what to test
+// describe('Sample Test', () => {
+//   // Test scenario
+//   it('runs jest', () => {
+//     expect(5).toBe(5)
+//   })
+//   // Test scenario
+//   it('checks array', () => {
+//     expect([1, 2, 3]).toEqual([1, 2, 3])
+//   })
+// })
